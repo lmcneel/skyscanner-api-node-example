1e570a50359f7f084a34e2e4600e0d5b12090df9
Tish McNeel <latisha.mcneel@gmail.com>
2016-12-11 13:05:09 -0600

    Initialize Get Request to Browser Cache of SkyScanner API

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # skyscanner-api-node-example
+
+SkyScanner API get example for Browse Cache
+1. Open up index.js and add your api key
+2. Public/index.html makes an API call where you can alter the parameters for starting location, destination, currency, locale, Outbound Departure time and Inbound Departure Time
+3. After cloning, run `npm install` to install all dependencies required
+3. Start the express server with `node index.js`
+4. Browse to the url with localhost:3000/index.html
---
+++ index.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var request = require("request");
+
+
+var app = express();
+
+// Put your skyscanner api key here
+var api_key = "la992349893483139715787446358156";
+
+
+// This will serve up any static files in the public folder
+app.use(express.static('public'));
+
+
+// This allows Cross-Origin Requests to our server
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+// Make sure everything works right
+app.get('/', function(req, res){
+    res.send("Hello world");
+});
+
+// Make a get call to the edamam api
+// Make sure you set app_key and api_key up above
+app.get('/api/skyscanner/:market/:locale/:location/:destination/:currency/:startdate/:enddate', function(req, res){
+    var params = req.params;
+    var market = (params.market) ? params.market : "GB";
+    var locale = (params.locale) ? params.locale : "en-gb";
+    var location = (params.location) ? params.location : "UK";
+    var destination = (params.destination) ? params.destination : "anywhere";
+    var currency = (params.currency) ? params.currency : "GBP";
+    var startdate = (params.startdate) ? params.startdate : "anytime";
+    var enddate = (params.enddate) ? params.enddate : "anytime";
+    var responseData;
+    request(
+      { method: 'GET'
+      , uri: 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/' + market + '/' + currency + '/' + locale + '/' + location + '/' + destination + '/' + startdate + '/' + enddate + '?apiKey=' + api_key
+      , gzip: true
+      }
+    , function (error, response, body) {
+        // body is the decompressed response body
+        console.log('server encoded the data as: ' + (response.headers['content-encoding'] || 'identity'));
+        console.log('the decoded data is: ' + body);
+        res.json(body);
+    });
+
+});
+
+app.listen(3000, function(){
+    console.log("Example app listening on port 3000!");
+})
